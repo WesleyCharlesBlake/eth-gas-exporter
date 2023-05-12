53aef27361391544c7615071fc89ea2bb5ac67d6
Wesley Charles Blake <wesley@nikordi.com>
2023-05-12 14:54:36 +0200

    refactor the exporter to use etherscane API

--- main_test.go
+++ main_test.go
@@ -1,77 +1,31 @@
 package main
 
 import (
-	"encoding/json"
-	"io/ioutil"
 	"net/http"
+	"net/http/httptest"
 	"testing"
+
+	"github.com/prometheus/client_golang/prometheus/testutil"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestGetETHGasPrices(t *testing.T) {
-	// Create a new HTTP client.
-	client := &http.Client{}
+func TestFetchGasPrices(t *testing.T) {
+	// Mock Etherscan API response
+	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		rw.Write([]byte(`{"status":"1","message":"OK","result":{"SafeGasPrice":"44","ProposeGasPrice":"44","FastGasPrice":"46"}}`))
+	}))
+	defer server.Close()
 
-	// Make a request to the Ethereum blockchain.
-	resp, err := client.Get("https://ethgasstation.info/api/ethgasAPI.json")
-	if err != nil {
-		t.Fatal(err)
-	}
+	// Temporarily replace EtherscanAPIURL with our mock server's URL
+	oldURL := EtherscanAPIURL
+	EtherscanAPIURL = server.URL
+	defer func() { EtherscanAPIURL = oldURL }() // restore original URL after test
 
-	// Read the response body.
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
+	// Call fetchGasPrices
+	fetchGasPrices()
 
-	// Unmarshal the JSON response into a map of gas prices.
-	gasPrices := make(map[string]interface{})
-	err = json.Unmarshal(body, &gasPrices)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// Get the gas prices from the map.
-	fastPrice, ok := gasPrices["fast"].(float64)
-	if !ok {
-		t.Errorf("Cannot convert fastPrice to float64")
-	}
-	fastestPrice, ok := gasPrices["fastest"].(float64)
-	if !ok {
-		t.Errorf("Cannot convert fastestPrice to float64")
-	}
-	safeLowPrice, ok := gasPrices["safeLow"].(float64)
-	if !ok {
-		t.Errorf("Cannot convert safeLowPrice to float64")
-	}
-	averagePrice, ok := gasPrices["average"].(float64)
-	if !ok {
-		t.Errorf("Cannot convert averagePrice to float64")
-	}
-
-	// Check that the gas prices are valid.
-	if fastPrice <= 0 {
-		t.Errorf("fastPrice must be greater than 0")
-	}
-	if fastestPrice <= 0 {
-		t.Errorf("fastestPrice must be greater than 0")
-	}
-	if safeLowPrice <= 0 {
-		t.Errorf("safeLowPrice must be greater than 0")
-	}
-	if averagePrice <= 0 {
-		t.Errorf("averagePrice must be greater than 0")
-	}
-
-	// Get the gas prices from the GetETHGasPrices function.
-	actualGasPrices, err := GetETHGasPrices()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// Check that the gas prices from the two functions are equal.
-	for key, value := range actualGasPrices {
-		if value != gasPrices[key] {
-			t.Errorf("Expected gas price for %s to be %f, got %f", key, gasPrices[key], value)
-		}
-	}
+	// Check that the metrics were correctly updated
+	assert.Equal(t, 44.0, testutil.ToFloat64(gasPrice.WithLabelValues("safe")))
+	assert.Equal(t, 44.0, testutil.ToFloat64(gasPrice.WithLabelValues("propose")))
+	assert.Equal(t, 46.0, testutil.ToFloat64(gasPrice.WithLabelValues("fast")))
 }
